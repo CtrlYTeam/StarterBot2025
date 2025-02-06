94b31d06bd509b0cdefbc06c1858ab9e30a2cb43
hchkrdtn <za4rynsky@gmail.com>
2025-02-05 19:13:57 -0600

    Create StarterKitControllers.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/StarterKitControllers.java
@@ -0,0 +1,216 @@
+/*   MIT License
+ *   Copyright (c) [2024] [Base 10 Assets, LLC]
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+@TeleOp(name="FTC Starter Kit Example Robot (INTO THE DEEP)", group="Robot")
+//@Disabled
+public class ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    public DcMotor  leftDrive   = null; //the left drivetrain motor
+    public DcMotor  rightDrive  = null; //the right drivetrain motor
+    public DcMotor  armMotor    = null; //the arm motor
+    public CRServo  intake      = null; //the active intake servo
+    public Servo    wrist       = null; //the wrist servo
+
+    final double ARM_TICKS_PER_DEGREE =
+            28 // number of encoder ticks per rotation of the bare motor
+                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
+                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
+                    * 1/360.0; // we want ticks per degree, not per rotation
+
+    final double ARM_COLLAPSED_INTO_ROBOT  = 0;
+    final double ARM_COLLECT               = 250 * ARM_TICKS_PER_DEGREE;
+    final double ARM_CLEAR_BARRIER         = 230 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SPECIMEN        = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SAMPLE_IN_LOW   = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_ATTACH_HANGING_HOOK   = 120 * ARM_TICKS_PER_DEGREE;
+    final double ARM_WINCH_ROBOT           = 15  * ARM_TICKS_PER_DEGREE;
+
+    /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements. */
+    final double INTAKE_COLLECT    = -1.0;
+    final double INTAKE_OFF        =  0.0;
+    final double INTAKE_DEPOSIT    =  0.5;
+
+    /* Variables to store the positions that the wrist should be set to when folding in, or folding out. */
+    final double WRIST_FOLDED_IN   = 0.8333;
+    final double WRIST_FOLDED_OUT  = 0.5;
+
+    /* A number in degrees that the triggers can adjust the arm position by */
+    final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
+
+    /* Variables that are used to set the arm to a specific position */
+    double armPosition = (int)ARM_COLLAPSED_INTO_ROBOT;
+    double armPositionFudgeFactor;
+
+    @Override
+    public void runOpMode() {
+        /*
+        These variables are private to the OpMode, and are used to control the drivetrain.
+         */
+        double left;
+        double right;
+        double forward;
+        double rotate;
+        double max;
+
+        /* Define and Initialize Motors */
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_front_drive"); //the left drivetrain motor
+        rightDrive = hardwareMap.get(DcMotor.class, "right_front_drive"); //the right drivetrain motor
+        armMotor   = hardwareMap.get(DcMotor.class, "left_arm"); //the arm motor
+
+        /* Most skid-steer/differential drive robots require reversing one motor to drive forward.
+        for this robot, we reverse the right motor.*/
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        /* Setting zeroPowerBehavior to BRAKE enables a "brake mode". This causes the motor to slow down
+        much faster when it is coasting. This creates a much more controllable drivetrain. As the robot
+        stops much quicker. */
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        /*This sets the maximum current that the control hub will apply to the arm before throwing a flag */
+        ((DcMotorEx) armMotor).setCurrentAlert(5,CurrentUnit.AMPS);
+
+        /* Before starting the armMotor. We'll make sure the TargetPosition is set to 0.
+        Then we'll set the RunMode to RUN_TO_POSITION. And we'll ask it to stop and reset encoder.*/
+        armMotor.setTargetPosition(0);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        /* Define and initialize servos.*/
+        intake = hardwareMap.get(CRServo.class, "intake");
+        wrist  = hardwareMap.get(Servo.class, "wrist");
+
+        /* Make sure that the intake is off, and the wrist is folded in. */
+        intake.setPower(INTAKE_OFF);
+        wrist.setPosition(WRIST_FOLDED_IN);
+
+        /* Send telemetry message to signify robot waiting */
+        telemetry.addLine("Robot Ready.");
+        telemetry.update();
+
+        /* Wait for the game driver to press play */
+        waitForStart();
+
+        /* Run until the driver presses stop */
+        while (opModeIsActive()) {
+
+            /* Set the drive and turn variables to follow the joysticks on the gamepad.
+            the joysticks decrease as you push them up. So reverse the Y axis. */
+            forward = -gamepad1.left_stick_y;
+            rotate  = gamepad1.right_stick_x;
+
+            left  = forward + rotate;
+            right = forward - rotate;
+
+            /* Normalize the values so neither exceed +/- 1.0 */
+            max = Math.max(Math.abs(left), Math.abs(right));
+            if (max > 1.0)
+            {
+                left /= max;
+                right /= max;
+            }
+
+            /* Set the motor power to the variables we've mixed and normalized */
+            leftDrive.setPower(left);
+            rightDrive.setPower(right);
+
+            if (gamepad1.a) {
+                intake.setPower(INTAKE_COLLECT);
+            }
+            else if (gamepad1.x) {
+                intake.setPower(INTAKE_OFF);
+            }
+            else if (gamepad1.b) {
+                intake.setPower(INTAKE_DEPOSIT);
+            }
+
+            if(gamepad1.right_bumper){
+                /* This is the intaking/collecting arm position */
+                armPosition = ARM_COLLECT;
+                wrist.setPosition(WRIST_FOLDED_OUT);
+                intake.setPower(INTAKE_COLLECT);
+                }
+
+                else if (gamepad1.left_bumper){
+                    /* This is about 20° up from the collecting position to clear the barrier
+                    Note here that we don't set the wrist position or the intake power when we
+                    select this "mode", this means that the intake and wrist will continue what
+                    they were doing before we clicked left bumper. */
+                    armPosition = ARM_CLEAR_BARRIER;
+                }
+
+                else if (gamepad1.y){
+                    /* This is the correct height to score the sample in the LOW BASKET */
+                wrist.setPosition(WRIST_FOLDED_OUT);
+
+            }
+
+                else if (gamepad1.dpad_left) {
+                    /* This turns off the intake, folds in the wrist, and moves the arm
+                    back to folded inside the robot. This is also the starting configuration */
+                    armPosition = ARM_COLLAPSED_INTO_ROBOT;
+                    intake.setPower(INTAKE_OFF);
+                    wrist.setPosition(WRIST_FOLDED_IN);
+                }
+
+                else if (gamepad1.dpad_right){
+                    /* This is the correct height to score SPECIMEN on the HIGH CHAMBER */
+                    armPosition = ARM_SCORE_SPECIMEN;
+                }
+
+                else if (gamepad1.dpad_up){
+                    /* This sets the arm to vertical to hook onto the LOW RUNG for hanging */
+                    armPosition = ARM_ATTACH_HANGING_HOOK;
+                    intake.setPower(INTAKE_OFF);
+                    wrist.setPosition(WRIST_FOLDED_IN);
+                }
+
+                else if (gamepad1.dpad_down){
+                    /* this moves the arm down to lift the robot up once it has been hooked */
+                    armPosition = ARM_WINCH_ROBOT;
+                    intake.setPower(INTAKE_OFF);
+                    wrist.setPosition(WRIST_FOLDED_IN);
+            }
+
+
+            /* Here we create a "fudge factor" for the arm position.
+            This allows you to adjust (or "fudge") the arm position slightly with the gamepad triggers. */
+
+            armPositionFudgeFactor = FUDGE_FACTOR * (gamepad1.right_trigger + (-gamepad1.left_trigger));
+
+
+            /* Here we set the target position of our arm to match the variable that was selected
+            by the driver.
+            We also set the target velocity (speed) the motor runs at, and use setMode to run it.*/
+            armMotor.setTargetPosition((int) (armPosition + armPositionFudgeFactor));
+
+            ((DcMotorEx) armMotor).setVelocity(2100);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            /* Check to see if our arm is over the current limit, and report via telemetry. */
+            if (((DcMotorEx) armMotor).isOverCurrent()){
+                telemetry.addLine("MOTOR EXCEEDED CURRENT LIMIT!");
+            }
+
+            /* send telemetry to the driver of the arm's current position and target position */
+            telemetry.addData("armTarget: ", armMotor.getTargetPosition());
+            telemetry.addData("arm Encoder: ", armMotor.getCurrentPosition());
+            telemetry.update();
+
+        }
+    }
+}
